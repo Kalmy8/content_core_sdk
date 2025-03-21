b7b1b9c56ef9bec5d66017a4734946726b085625
Kalmy8 <kalmykovalexey01@gmail.com>
2025-03-21 15:14:33 +0300

    fix: fix type hints

--- content_core_sdk/kafka_client/models/text_generation_models.py
+++ content_core_sdk/kafka_client/models/text_generation_models.py
@@ -8,7 +8,7 @@ class ContentRequestsText(ISerializable):
     feature: str | None = Field(None, description="Producer method name", example="reply_tweet")
     correlation_id: str | None = Field(None, description="Unique ID for request-response correlation", example="123e4567-e89b-12d3-a456-426614174000")
     user_id: str | None = Field(None, description="ID of the requesting user", example="user_12345")
-    character_name: str = Field(None, description="Name of the character for text generation", example="Trump")
+    character_name: str | None = Field(None, description="Name of the character for text generation", example="Trump")
     request_text: str = Field(..., description="Text prompt for generating the answer", example="What are your intentions for the presidency?")
     task_description: str = Field(..., description="Additional context for the text generation task", example="You are an AI-powered chatbot for a website, be creative and entertaining in your responses")
 
